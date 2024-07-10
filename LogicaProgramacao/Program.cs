1e1d7acdf28efe7958c3afef7631649697d1a5e5
Grace <166858250+Grace-Garces@users.noreply.github.com>
2024-07-10 10:25:03 -0300

    Add files via upload

---
+++ LogicaProgramacao/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        Console.WriteLine("Entre com seu nome completo");
+        string Nome = Console.ReadLine();
+
+        Console.WriteLine("Quantos quartos tem na sua casa?");
+        int Quartos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com o preço de um produto");
+        double Preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre seu último nome, idade e altura");
+
+        string[] vet = Console.ReadLine().Split(' ');
+        string Sobrenome = vet[0];
+        int idade = int.Parse(vet[1]);
+        double altura = double.Parse(vet[2]);
+
+     Console.WriteLine();
+     Console.WriteLine(Nome);
+     Console.WriteLine(Quartos);
+     Console.WriteLine(Preco);
+     Console.WriteLine(Sobrenome);
+     Console.WriteLine(idade);
+     Console.WriteLine(altura);
+
+
+
+            Console.ReadLine();
+        
+    }
+}
